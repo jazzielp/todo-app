5f02837cea9b92c5c49414416c8f9c830015e47a
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-04-22 17:38:17 -0700

    🎨 Create HTML and Create icons component

--- src/design/Design.jsx
+++ src/design/Design.jsx
@@ -2,6 +2,44 @@ import React from 'react'
 
 export function Design () {
   return (
-    <header />
+    <div className='app'>
+      <header className='header'>
+        <h1 className='header__title'>TODO</h1>
+        <Moon />
+      </header>
+      <main className='main'>
+        <div className='new-todo'>
+          <input className='new-todo__input' type='text' name='create-todo' />
+        </div>
+        <div className='list-todo'>
+          <div className='list-todo__item'>
+            <div className='list-todo__info-container'>
+              <div className='list-todo__icon-' />
+              <p className='list-todo__todo'>Reunión a las 5</p>
+            </div>
+            <Cross />
+          </div>
+          <div className='list-todo__actions'>
+            <p className='list-todo__count-todo'>5 items left</p>
+            <div className='list-todo__filters'>
+              <a href='#' className='list-todo-filter'>All</a>
+              <a href='#' className='list-todo-filter'>Active</a>
+              <a href='#' className='list-todo-filter'>Completed</a>
+            </div>
+            <div className='list-todo__clear-container'>
+              <a href='#' className='list-todo__clear-completed'>Clear Completed</a>
+            </div>
+          </div>
+        </div>
+        <div className='list-todo__filters-moviel'>
+          <a href='#' className='filters-movie-filter'>All</a>
+          <a href='#' className='filters-movie-filter'>Active</a>
+          <a href='#' className='filters-movie-filter'>Completed</a>
+        </div>
+      </main>
+      <footer className='footer'>
+        <h3>Drag and drop to reorder list</h3>
+      </footer>
+    </div>
   )
 }
