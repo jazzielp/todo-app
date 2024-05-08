579c9a2a425b907c6a3ec40abc5113c544b0ce32
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-05-08 16:08:52 -0700

    🐛 Fix filters

--- src/components/ListTodo.tsx
+++ src/components/ListTodo.tsx
@@ -2,14 +2,16 @@ import { useTodo } from '../hooks/useTodo'
 import { ItemTodo } from './ItemTodo'
 import { Todo } from '../types/types'
 import { Actions } from './Actions'
+import { FILTERS } from '../const/const'
 export function ListTodo (): JSX.Element {
-  const { todos, filtersTodos } = useTodo()
+  const { todos, filtersTodos, filter } = useTodo()
+
   return (
     <div className='list-todo'>
       {
-        filtersTodos?.length > 0
-          ? filtersTodos && filtersTodos.map((todo: Todo) => <ItemTodo key={todo.id} todo={todo} />)
-          : todos && todos.map((todo: Todo) => <ItemTodo key={todo.id} todo={todo} />)
+        filter === FILTERS.All
+          ? todos && todos.map((todo: Todo) => <ItemTodo key={todo.id} todo={todo} />)
+          : filtersTodos && filtersTodos.map((todo: Todo) => <ItemTodo key={todo.id} todo={todo} />)
       }
       <Actions />
     </div>
