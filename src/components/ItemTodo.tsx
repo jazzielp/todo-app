8428fc99376facc9f90a65158add15a5bea72734
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-05-15 17:22:12 -0700

    ksajdkjasd

--- src/components/ItemTodo.tsx
+++ src/components/ItemTodo.tsx
@@ -1,3 +1,4 @@
+import { DragEvent } from 'react'
 import { Cross } from './icons/Cross'
 import { Todo } from '../types/types'
 import { Check } from './icons/Check'
@@ -9,16 +10,42 @@ interface ItemTodoProps {
 
 export function ItemTodo ({ todo }: ItemTodoProps): JSX.Element {
   const { id, title, completed } = todo
-  const { todoCompleted, deleteTodo } = useTodo()
+  const { todoCompleted, deleteTodo, todos, setTodos } = useTodo()
   const handleCommpleted = (id: string): void => {
     todoCompleted(id)
   }
 
-  const handleDelete = (id: string) => {
+  const handleDelete = (id: string): void => {
     deleteTodo(id)
   }
+
+  const startDarg = (event: DragEvent<HTMLDivElement>, todo: Todo): void => {
+    event.dataTransfer.setData('todoID', todo.id)
+  }
+
+  const dragginOver = (event: DragEvent<HTMLDivElement>): void => {
+    event.preventDefault()
+  }
+
+  const onDrop = (event: DragEvent<HTMLDivElement>, todoReplace: Todo): void => {
+    const todoID = event.dataTransfer.getData('todoID')
+    const todoDrag = todos.find((todo: Todo) => todo.id === todoID)
+    const todoDragIndex = todos.findIndex((todo: Todo) => todo.id === todoID)
+    const todoReplaceIndex = todos.findIndex((todo: Todo) => todo.id === todoReplace.id)
+    const newTodos = structuredClone(todos)
+    newTodos[todoReplaceIndex] = todoDrag
+    newTodos[todoDragIndex] = todoReplace
+    setTodos(newTodos)
+  }
   return (
-    <div className='list-todo__item'>
+    <div
+      className='list-todo__item'
+      draggable
+      droppable='true'
+      onDragOver={dragginOver}
+      onDragStart={event => startDarg(event, todo)}
+      onDrop={event => onDrop(event, todo)}
+    >
       <div className='list-todo__info-container'>
         {completed
           ? (
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,6 +3,9 @@ import { Filters } from './Filters'
 import { Form } from './Form'
 import { ListTodo } from './ListTodo'
 import { useTodo } from '../hooks/useTodo'
+import { DndContext, closestCenter } from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
+import { Todo } from '../types/types'
 
 const mockTodos = [
   {
@@ -23,20 +26,36 @@ const mockTodos = [
 ]
 
 export function Main (): JSX.Element {
-  const { setTodos } = useTodo()
+  const { todos, setTodos } = useTodo()
   useEffect(() => {
     const storageTodos = JSON.parse(localStorage.getItem('DATA_TODOS') || '[]')
-    if (storageTodos.length > 0) {
-      setTodos(storageTodos)
-    } else {
-      setTodos(mockTodos)
-    }
+    setTodos(storageTodos)
   }, [])
+
+  const handleDragEnd = (event: any): void => {
+    const { active, over, activatorEvent } = event
+    const classDiv = activatorEvent.target.className
+    console.log(classDiv)
+    console.log('entra')
+    const oldIndex = todos.findIndex((todo: Todo) => todo.id === active.id)
+    const newIndex = todos.findIndex((todo: Todo) => todo.id === over.id)
+    const newTodos = arrayMove(todos, oldIndex, newIndex)
+    setTodos(newTodos)
+  }
+
+  const handleCancelDrop = (event: any) => {
+    const classDiv = event.activatorEvent.target.className
+    console.log('entra')
+    return classDiv === 'list-todo__icon-check-brackground'
+  }
+
   return (
-    <main className='main'>
-      <Form />
-      <ListTodo />
-      <Filters />
-    </main>
+    <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd} cancelDrop={handleCancelDrop}>
+      <main className='main'>
+        <Form />
+        <ListTodo />
+        <Filters />
+      </main>
+    </DndContext>
   )
 }
